152b675d210f9015f2ae7e3b65a0b17db699d080
rawatakanksha <rawatakanksha2313@gmail.com>
2024-09-24 23:44:13 +0530

    Update main.js

--- 25/toDoList/main.js
+++ 25/toDoList/main.js
@@ -1,31 +1,6 @@
 // Interactive To-Do List: Create a simple to-do list app that allows users to add, remove, and mark tasks as completed.
 
-
-
-
-
-
-const list=document.querySelector('.listItems');
-let tasks=JSON.parse(localStorage.getItem("listItems"))
-function populateList(tasks=[],list){
-    list.innerHTML= tasks.map((tasks,i)=>{
-     return `<li><input type="checkbox" data-index=${i} id="tasks${i}"
-     ${tasks.done?"checked":""}/>
-     <label for="tasks${i}">${tasks.text}</label></li>`
-    }).join("");
- }
-
- function toggle(e){
-    if(!e.target.matches("input")) return;
-    const element=e.target;
-    const index=element.dataset.index;
-    tasks[index].done=!tasks[index].done;
-    localStorage.setItem("tasks",JASON.stringify(tasks))
-    populateList(tasks,listItem)
-    
- }
 const btn=document.querySelector('.button');
-const listItem=document.createElement('li');
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
     
@@ -36,35 +11,23 @@ btn.addEventListener('click',(e)=>{
     }
   else{
 
-    
-    
+    const list=document.querySelector('.listItems');
+    const listItem=document.createElement('li');
     listItem.textContent=taskInput;
 
-    
-    
-
-   
-
-    // const checkbox=document.createElement('input');
-    // checkbox.type='checkbox';
-    // listItem.appendChild(checkbox);
+    const checkbox=document.createElement('input');
+    checkbox.type='checkbox';
+    listItem.appendChild(checkbox);
 
     list.appendChild(listItem);
     input.value='';
-    populateList(tasks,listItem)
-
-    document.getElementsByClassName('listContainer')[0].style.display='block';
 
 //     document.querySelector('.listItems').innerHTML= 
 //     ` 
 //    <li> ${taskInput}</li>
 //    <input type="checkbox">   
 //     </div>`;
-
-
 }
-// document.getElementsByClassName('listContainer')[0].style.display='block';
+document.getElementsByClassName('listContainer')[0].style.display='block';
 
 })
-list.addEventListener("click",toggle);
-// populateList(tasks,listItem);
